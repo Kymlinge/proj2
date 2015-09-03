2bfdbbbd6a0baef2e1334aa211717923db1fb602
4vv9int <tannerthayer@gmail.com>
2015-09-02 19:46:02 -0600

    click enter to add text and click again to remove

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 var app = angular.module('app', []);
 
 app.controller('myCtrl', ['$scope', function($scope){
+	$scope.on = false;
 	$scope.name = "horrrr";
 	$scope.doSomething = function() {
-		console.log($scope.name);
+		var canvas = document.getElementById("horrorCanvas");
+		var context = canvas.getContext("2d");
+
+		if (!$scope.on) {
+			context.fillStyle = "red";
+			context.font = "bold 32px Arial";
+			context.fillText("Welcome", 100, 100);
+			$scope.on = true;
+		} else {
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			$scope.on = false;
+		}
+
 	}
 	document.getElementById("horrorCanvas").focus();
 }]);
 
 app.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
+	return function (scope, element, attrs) {
+		element.bind("keydown keypress", function (event) {
+			if(event.which === 13) {
+				scope.$apply(function (){
+					scope.$eval(attrs.ngEnter);
+				});
 
-                event.preventDefault();
-            }
-        });
-    };
+				event.preventDefault();
+			}
+		});
+	};
 });
 
 //maybe use a sound library?
